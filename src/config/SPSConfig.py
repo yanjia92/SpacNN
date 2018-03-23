be1b21bb123cbe86abe21d66de5946bda79f3668
TracyYan2015 <bokoryan@gmail.com>
2018-03-23 20:13:57 +0800

    修改了s3r的损坏阈值，使得S3R模块在厚度为4的时候损坏概率维持在一个较小的情况。所有的参数均以src/config/SPSConfig.py的为准

--- src/config/SPSConfig.py
+++ src/config/SPSConfig.py
@@ -6,11 +6,11 @@ class SPSConfig(object):
         self.params = {}
         self.params['SB_K'] = Constant('SB_K', 0.0039)
         self.params['SB_B'] = Constant('SB_B', 12)
-        self.params['SB_P_THRESHOLD'] = Constant('SB_P_THRESHOLD', 0.7)
+        self.params['SB_P_THRESHOLD'] = Constant('SB_P_THRESHOLD', 0.78)
         self.params['SB_A_MU'] = Constant('SB_A_MU', 0.1754)
         self.params['SB_A_SIGMA'] = Constant('SB_A_SIGMA', 0.02319029 * 21)
         self.params['S3R_K'] = Constant('S3R_K', 200.5 / 3.0)  # s3r, bdr, bcr三个模块均摊电离能损
-        self.params['S3R_DELTAV_THRESHOLD'] = Constant('S3R_DELTAV_THRESHOLD', 450)
+        self.params['S3R_DELTAV_THRESHOLD'] = Constant('S3R_DELTAV_THRESHOLD', 580)
         self.params['S3R_A_MU'] = Constant('S3R_A_MU', 570.8 * 18 - 570.8 * 5)
         self.params['S3R_A_SIGMA'] = Constant('S3R_A_SIGMA', 6.7471 * 120)
         self.params['S3R_B'] = Constant('S3R_B', 0.01731)
@@ -22,5 +22,8 @@ class SPSConfig(object):
             return self.params[name]
         else:
             raise Exception('no parameter found: {0}'.format(name))
+
+    def setParam(self, name, value):
+        self.params[name].value = value
 
 
--- src/test/testFailProbS3R.py
+++ src/test/testFailProbS3R.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from module.Module import *
-from math import *
 from util.MathUtils import *
 from util.util import *
 from model.ModuleFactory import ModuleFactory
 from config.SPSConfig import SPSConfig
 import unittest
 
+YEAR=5
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -16,14 +16,20 @@ class TestCase(unittest.TestCase):
     def tests3rfail(self):
         timer = self.modulefactory.timermodule()
         s3r = self.modulefactory.s3rmodule()
-        for v in [Variable("day", i) for i in interval(1, 365, 1)]:
-            dose = v.getValue() / 365.0 * (s3r.getConstant("S3R_K").getValue() /
-                                           self.config.getParam("SCREEN_THICKNESS").getValue())
-            x = s3r.getConstant("S3R_DELTAV_THRESHOLD").getValue() / (
-            s3r.getConstant("S3R_B").getValue() * exp(s3r.getConstant("S3R_B").getValue() * dose))
-            std_x = (x - s3r.getConstant("S3R_A_MU").getValue()) / s3r.getConstant("S3R_A_SIGMA").getValue()
-            p = 1 - pcf(std_x)
-            print p
+        ps = []
+        for t in range(1, 11):
+            self.config.setParam("SCREEN_THICKNESS", t)
+            for v in [Variable("day", i) for i in interval(1, 365*YEAR, 1)]:
+                dose = v.getValue() / 365.0 * (s3r.getConstant("S3R_K").getValue() /
+                                               self.config.getParam("SCREEN_THICKNESS").getValue())
+                x = s3r.getConstant("S3R_DELTAV_THRESHOLD").getValue() / (
+                s3r.getConstant("S3R_B").getValue() * exp(s3r.getConstant("S3R_B").getValue() * dose))
+                std_x = (x - s3r.getConstant("S3R_A_MU").getValue()) / s3r.getConstant("S3R_A_SIGMA").getValue()
+                p = 1 - pcf(std_x)
+                ps.append(p)
+            if t == 4:
+                print t
+                print ps[-5:]
 
 
 
